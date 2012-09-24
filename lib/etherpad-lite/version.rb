3f9eb16d6c27dad21a94ef887c2be00a6c1c5567
Jordan Hollinger <jordan.hollinger@gmail.com>
2012-09-24 00:07:44 -0400

    Simplify VERSION

--- lib/etherpad-lite/version.rb
+++ lib/etherpad-lite/version.rb
@@ -1,5 +1,4 @@
 module EtherpadLite
-  MAJOR_VERSION, MINOR_VERSION, TINY_VERSION, PRE_VERSION = 0, 2, 0, nil # :nodoc:
-  # The client version
-  VERSION = [MAJOR_VERSION, MINOR_VERSION, TINY_VERSION, PRE_VERSION].compact.join '.'
+  # The library version
+  VERSION = '0.2.0'
 end
